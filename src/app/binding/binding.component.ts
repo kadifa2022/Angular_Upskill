6f97b8c53576c0d3a11fddb2c565a941830666e6
Kadifa <Kudickadifa@gmail.com>
2023-08-02 17:03:02 -0500

    angular_upskill

---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent {
+
+  currentCustomer : string = 'Maria';
+  word : number = 1200;
+  isActive : boolean = true;
+
+  constructor(private router: Router){
+
+  }
+
+  onClick(event : any){
+   this.isActive = !this.isActive;
+    console.log(event);
+ }
+ onMouseOver(){
+  alert("Don't do it please...");
+ }
+
+ onInputChange(event : any){
+  console.log(event.target.value);
+  this.currentCustomer = event.target.value;
+  
+ }
+
+ //--------------------------------
+ inputString: string = 'This is input';
+ 
+ onSubmit(value: string){
+  this.inputString = value;
+ }
+ //------------router---------
+ goToDirectives(){
+  this.router.navigate(['directives']);
+
+ }
+
+}
