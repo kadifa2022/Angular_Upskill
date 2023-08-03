fdc9e865d04b9173fb6f92970b8b7637c70701b1
Kadifa <Kudickadifa@gmail.com>
2023-08-03 16:51:33 -0500

    comm

--- src/app/xcomm/parent/parent.component.ts
+++ src/app/xcomm/parent/parent.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit{
+mentors: string [] = ['Ower', 'Mat', 'Sahin'];
+mentorSelected: string ='Ower';
+constructor(){}
+
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSelectedMentor(mentor: string){
+    this.mentorSelected = mentor;
+    console.log(this.mentorSelected);
+
+  }
+
+
 
 }
